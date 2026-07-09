03ebe8bea8be55af7d2d231da343065c91f3d879
isabbzinha <isabelli.bertao@escola.pr.gov.br>
2026-07-09 09:23:37 -0300

    salvar

---
+++ script.js
@@ -0,0 +1,8 @@
+const curtidas = document.getElementById("curtidas") // Pega o botão de curtidas pelo id
+curtidas.addEventListener("click", aumentarCurtidas) // quando clicar chama a função
+
+// função de aumentar curtidas
+function aumentarCurtidas(){
+    let quantidade = document.querySelector("span")
+    quantidade.textContent++;
+}
